d2e1aa61bd14ba0ae37cbe54ed24340ecebf4092
João Vitor Curtarelli Benini <joao.curtarelli@beatscode.com>
2024-06-03 19:43:22 -0300

    adjust

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,8 @@ import Layout from './components/Layout'
 import Home from './pages/Home'
 import DonorCreate from './pages/Donor/Register'
 import DonorList from './pages/Donor/List'
+import DonaratyList from './pages/Donatary/List'
+import DonaratyCreate from './pages/Donatary/Register'
 
 const router = createBrowserRouter([
   {
@@ -13,6 +15,9 @@ const router = createBrowserRouter([
 
       { path: 'donor', element: <DonorList /> },
       { path: 'donor-register/:id?', element: <DonorCreate /> },
+
+      { path: 'donatary', element: <DonaratyList /> },
+      { path: 'donatary-register/:id?', element: <DonaratyCreate /> },
     ],
   },
 ])
